37a6c5092773eea2a5b436a733d0d69433e906db
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-10-26 09:47:05 +0530

    helloworld.go

---
+++ README.md
@@ -0,0 +1,2 @@
+# Go
+![Go Lang](https://miro.medium.com/max/3000/1*30aoNxlSnaYrLhBT0O1lzw.png "Go Lang")
---
+++ src/helloworld.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world.\nThis is my first Go program and just like any other language hello world program is the first one that I have coded and executed.")
+}
